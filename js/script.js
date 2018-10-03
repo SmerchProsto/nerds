9f449ace9a988e3a05cab0423599f841a851c0a5
SmerchProsto <smerchprosto@gmail.com>
2018-10-03 18:56:23 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-1
+const contactButton = document.querySelector(".contact-button");
+const popup = document.querySelector(".popup");
+const popupButtonInner = document.querySelector(".popup-button-inner");
+var html = document.getElementsByTagName("html");
+
+function addClassToTag(classNameTag, tag) {
+	tag.classList.add(classNameTag);
+}
+
+function showBlock(tag, tag2, classNameTag) {
+	tag.addEventListener("click", function(event) {
+		event.preventDefault();
+		addClassToTag(classNameTag, tag2);
+	});
+}
+
+function closeBlock(tag, tag2, classNameTag, htmlDocument) {
+	tag.addEventListener("click", function(event) {
+		event.preventDefault();
+		tag2.classList.remove(classNameTag);
+	});
+}
+
+showBlock(contactButton, popup, "show-popup");
+closeBlock(popupButtonInner, popup, "show-popup", html);
